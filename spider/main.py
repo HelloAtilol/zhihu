d0e959f65d7081de47626b8c411c07be7d8519ab
HelloAtilol <41175733+HelloAtilol@users.noreply.github.com>
2020-01-06 16:10:15 +0800

    Update main.py

--- spider/main.py
+++ spider/main.py
@@ -9,7 +9,7 @@ import time
 import random
 
 # 建立数据库连接
-conn = pymysql.Connect(host='localhost', port=3306, user='root', passwd='sim509', db='zhihu', charset='utf8')
+conn = pymysql.Connect(host='localhost', port=3306, user='root', passwd='*****', db='zhihu', charset='utf8')
 cursor = conn.cursor()
 
 header = {
